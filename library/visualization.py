c0a5504bedfe3f6a4f13ca3fc42d583a08cb016f
Robotmurlock <momir.adzemovic@gmail.com>
2021-08-29 20:30:49 +0200

    Dodate funkcije za vizualizaciju rezultata

--- library/visualization.py
+++ library/visualization.py
@@ -25,3 +25,44 @@ def test_image_generation(gen_model, input_batch, target_batch, img_size, rows=4
             axs[i][j].set_title(title)
             axs[i][j].axis('off')
     plt.show()
+
+
+def plot_history(history, axs, col, prefix=''):
+    """
+    :param history: Istorija funkcije greske
+    :param axs: Grafice nad kojim se slika
+    :param col: Kolona koja se popunjava (ako ima vise skupova, moze da se gleda uporedo rezultat)
+    :param prefix: Prefiks za rezultat u izabranoj koloni
+    """
+    gen_total_loss, gen_gan_loss, gen_l1_loss, disc_loss = zip(*history)
+    axs[0][col].plot(gen_total_loss, color='red', label='Total Loss')
+    axs[0][col].plot(gen_gan_loss, color='blue', label='GEN Loss')
+    axs[0][col].plot(gen_l1_loss, color='green', label='L1 Loss')
+    axs[0][col].set_title(f'{prefix} GAN Loss')
+    axs[0][col].set_xlabel('Step')
+    axs[0][col].set_ylabel('Loss')
+    axs[0][col].legend()
+
+    axs[1][col].plot(disc_loss, color='red')
+    axs[1][col].set_title(f'{prefix} DISC Loss')
+    axs[1][col].set_xlabel('Step')
+    axs[1][col].set_ylabel('Loss')
+
+    axs[2][col].plot(disc_loss, color='red', label='DISC Loss')
+    axs[2][col].plot(gen_gan_loss, color='blue', label='GEN Loss')
+    axs[2][col].set_title(f'{prefix} DISC vs GEN Loss')
+    axs[2][col].set_xlabel('Step')
+    axs[2][col].set_ylabel('Loss')
+    axs[2][col].legend()
+
+
+def plot_training_results(train_history, val_history):
+    """
+    Prikazuje grafike koji predstavljaju rezultate treniranja nad skupom za ucenje i nad skupom za validaciju.
+    :param train_history: Istorija funkcije greske nad skupom za ucenje
+    :param val_history: Istorija funkcije greske nad skupom za validaciju
+    """
+    fig, axs = plt.subplots(figsize=(12, 10), nrows=3, ncols=2)
+    plot_history(train_history, axs, col=0, prefix='Train:')
+    plot_history(val_history, axs, col=1, prefix='Validation:')
+    plt.tight_layout()
